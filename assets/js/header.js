c7bb203c906594c5f553af914402bff7dfdbe8dc
Zibing Zhang <zibing.zha@gmail.com>
2024-01-09 17:48:23 -0500

    Fix UI bug

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,13 +2,18 @@
 
 for (const element of document.getElementsByClassName("toggle")) {
   element.onclick = _ => {
-    const target = document.getElementById(element.dataset.for);
-    if (target.dataset.active === "false") {
+    if (element.dataset.active === "false") {
       element.dataset.active = "true";
-      target.dataset.active = "true";
     } else {
       element.dataset.active = "false"
-      target.dataset.active = "false";
+    }
+    const targets = document.getElementsByClassName(element.dataset.for);
+    for (const target of targets) {
+      if (target.dataset.active === "false") {
+        target.dataset.active = "true";
+      } else {
+        target.dataset.active = "false";
+      }
     }
   };
 }
